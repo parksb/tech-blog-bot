af8ee0e2c6dcd3fda9817535d53157379cea101c
parksb <parkgds@gmail.com>
2025-04-05 16:02:58 +0900

    update feeds

--- feeds.ts
+++ feeds.ts
@@ -1,12 +1,212 @@
 export const FEEDS = [
   {
+    title: "11번가",
+    url: "https://11st-tech.github.io/rss",
+    language: "ko",
+  },
+  {
+    title: "29CM",
+    url: "https://medium.com/feed/29cm",
+    language: "ko",
+  },
+  {
+    title: "44BITS",
+    url: "https://www.44bits.io/ko/feed/all",
+    language: "ko",
+  },
+  {
+    title: "네이버",
+    url: "https://d2.naver.com/d2.atom",
+    language: "ko",
+  },
+  {
+    title: "에잇퍼센트",
+    url: "https://8percent.github.io/feed.xml",
+    language: "ko",
+  },
+  {
+    title: "AWS",
+    url: "https://aws.amazon.com/ko/blogs/korea/feed",
+    language: "ko",
+  },
+  {
+    title: "리멤버",
+    url: "https://tech.remember.co.kr/feed",
+    language: "ko",
+  },
+  {
+    title: "LY Corporation",
+    url: "https://techblog.lycorp.co.jp/ko/feed/index.xml",
+    language: "ko",
+  },
+  {
+    title: "NHN Cloud",
+    url: "https://meetup.nhncloud.com/rss",
+    language: "ko",
+  },
+  {
+    title: "pxd",
+    url: "https://pxdstory.tistory.com/rss",
+    language: "ko",
+  },
+  {
+    title: "SK C&C",
+    url: "https://engineering-skcc.github.io/feed.xml",
+    language: "ko",
+  },
+  {
+    title: "SK플래닛",
+    url: "https://techtopic.skplanet.com/rss.xml",
+    language: "ko",
+  },
+  {
+    title: "쏘카",
+    url: "https://tech.socarcorp.kr/feed.xml",
+    language: "ko",
+  },
+  {
+    title: "SSG",
+    url: "https://medium.com/feed/ssgtech",
+    language: "ko",
+  },
+  {
+    title: "왓챠",
+    url: "https://medium.com/feed/watcha",
+    language: "ko",
+  },
+  {
+    title: "개발자스럽다",
+    url: "https://blog.gaerae.com/feeds/posts/default",
+    language: "ko",
+  },
+  {
+    title: "NDS",
+    url: "https://tech.cloud.nongshim.co.kr/feed",
+    language: "ko",
+  },
+  {
+    title: "당근",
+    url: "https://medium.com/feed/daangn",
+    language: "ko",
+  },
+  {
+    title: "데브시스터즈",
+    url: "https://tech.devsisters.com/rss.xml",
+    language: "ko",
+  },
+  {
+    title: "레몬베이스",
+    url: "https://blog.lemonbase.team/feed",
+    language: "ko",
+  },
+  {
+    title: "롯데ON",
+    url: "https://techblog.lotteon.com/feed",
+    language: "ko",
+  },
+  {
+    title: "리디",
+    url: "https://ridicorp.com/feed",
+    language: "ko",
+  },
+  {
+    title: "모두싸인",
+    url: "https://team.modusign.co.kr/feed",
+    language: "ko",
+  },
+  {
+    title: "무신사",
+    url: "https://medium.com/feed/musinsa-tech",
+    language: "ko",
+  },
+  {
+    title: "뱅크샐러드",
+    url: "https://blog.banksalad.com/rss.xml",
+    language: "ko",
+  },
+  {
+    title: "올리브영",
+    url: "https://oliveyoung.tech/rss.xml",
+    language: "ko",
+  },
+  {
+    title: "우아한형제들",
+    url: "https://techblog.woowahan.com/feed",
+    language: "ko",
+  },
+  {
+    title: "인프랩",
+    url: "https://tech.inflab.com/rss.xml",
+    language: "ko",
+  },
+  {
+    title: "카카오엔터테인먼트 FE",
+    url: "https://fe-developers.kakaoent.com/rss.xml",
+    language: "ko",
+  },
+  {
+    title: "컬리",
+    url: "http://thefarmersfront.github.io/feed.xml",
+    language: "ko",
+  },
+  {
+    title: "컴투스",
+    url: "https://on.com2us.com/category/tech/rss",
+    language: "ko",
+  },
+  {
+    title: "테이블링",
+    url: "https://techblog.tabling.co.kr/feed",
+    language: "ko",
+  },
+  {
+    title: "티빙",
+    url: "https://medium.com/feed/tving-team",
+    language: "ko",
+  },
+  {
     title: "토스",
     url: "https://toss.tech/rss.xml",
-    language: "en",
+    language: "ko",
   },
   {
     title: "카카오",
     url: "https://tech.kakao.com/feed",
+    language: "ko",
+  },
+  {
+    title: "하이퍼커넥트",
+    url: "https://hyperconnect.github.io/feed.xml",
+    language: "ko",
+  },
+  {
+    title: "한국신용데이터",
+    url: "https://www.blog.kcd.co.kr/feed",
+    language: "ko",
+  },
+  {
+    title: "PRND",
+    url: "https://medium.com/feed/prnd",
+    language: "ko",
+  },
+  {
+    title: "Spotify",
+    url: "https://engineering.atspotify.com/feed",
+    language: "en",
+  },
+  {
+    title: "Stack Overflow",
+    url: "https://stackoverflow.blog/engineering/feed",
+    language: "en",
+  },
+  {
+    title: "GitHub",
+    url: "https://github.blog/feed",
+    language: "en",
+  },
+  {
+    title: "Netflix",
+    url: "https://netflixtechblog.com/feed",
     language: "en",
   },
 ];
